d20099cc45ebaa65faab57394c407461ffd39db1
dirkwoellhaf <dirk.woellhaf@gmail.com>
2016-06-02 19:03:27 +0200

    bd

--- add_tenant.py
+++ add_tenant.py
@@ -20,7 +20,7 @@ json_credentials = json.dumps(name_pwd)
 login_url = base_url + 'aaaLogin.json'
 post_response = requests.post("https://10.1.6.120/api/aaaLogin.json", data=json.dumps(name_pwd), verify=False)
 
-#print post_response
+#print post_response.text
 
 
 # get token from login response structure
@@ -32,9 +32,12 @@ auth_token = login_attributes['token']
 cookies = {}
 cookies['APIC-Cookie'] = auth_token
 
+
+#BD
+bd = {"totalCount":"1","imdata":[{"fvBD":{"attributes":{"arpFlood":"no","descr":"","dn":"uni/tn-A40_n900/BD-core_bd_2","epMoveDetectMode":"","limitIpLearnToSubnets":"no","llAddr":"::","mac":"00:22:BD:F8:19:FF","multiDstPktAct":"bd-flood","name":"core_bd_2","ownerKey":"","ownerTag":"","unicastRoute":"no","unkMacUcastAct":"proxy","unkMcastAct":"flood","vmac":"not-applicable"},"children":[{"fvRsBDToNdP":{"attributes":{"tnNdIfPolName":""}}},{"fvRsCtx":{"attributes":{"tnFvCtxName":"n900_vrf_prod"}}},{"fvRsIgmpsn":{"attributes":{"tnIgmpSnoopPolName":""}}},{"fvRsBdToEpRet":{"attributes":{"resolveAct":"resolve","tnFvEpRetPolName":""}}}]}}]}
 # read a sensor, incorporating token in request
-sensor_url = base_url + 'mo/topology/pod-1/node-101/sys/ch/bslot/board/sensor-3.json'
-get_response = requests.get(sensor_url, cookies=cookies, verify=False)
+sensor_url = base_url + 'mo.json'
+get_response = requests.post(sensor_url, cookies=cookies,data=json.dumps(bd), verify=False)
 
 # display sensor data structure
 print get_response.json()
